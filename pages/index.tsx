c487bf6100f4d71733e03ed87d95aa9e2c359d11
unknown <twomeaningsof@gmail.com>
2023-01-08 19:57:32 +0100

    chore (project): setup

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+
+interface CharactersPageProps {
+	characters: object[];
+}
+
+const CharactersPage = (props: CharactersPageProps) => {
+	return (
+		<div className='w-full bg-gray-500 text-[14px]'>
+			Characters page:
+			<ul>{props.characters[0].name}</ul>
+		</div>
+	);
+};
+
+export default CharactersPage;
+
+export async function getStaticProps() {
+	const { data } = await client.query({
+		query: gql`
+			query Characters {
+				characters(filter: { name: "Mechanical Rick" }) {
+					results {
+						id
+						name
+						status
+					}
+				}
+			}
+		`,
+	});
+
+	return {
+		props: {
+			characters: data.characters.results,
+		},
+	};
+}
