5e144b7ac2f79f759c7bd7b65ceaf2eacc7db218
unknown <twomeaningsof@gmail.com>
2023-01-10 22:45:54 +0100

    chore:graphql-codegen, apollo provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,33 @@
-import { gql } from "@apollo/client";
-import client from "../../apollo-client";
+import { useQuery } from "@apollo/client";
+import { graphql } from "../gql/";
 
-interface CharactersPageProps {
-  characters: object[];
-}
+const GET_CHARACTERS = graphql(`
+  query Characters {
+    characters(filter: { name: "Mechanical Rick" }) {
+      results {
+        id
+        name
+        status
+      }
+    }
+  }
+`);
 
-const CharactersPage = (props: CharactersPageProps) => {
+const CharactersPage = () => {
+  const { loading, error, data } = useQuery(GET_CHARACTERS);
+
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div className='w-full bg-gray-500 text-[14px]'>
       Characters page:
-      <ul>{props.characters[0].name}</ul>
+      <ul>
+        {data?.characters?.results &&
+          data.characters.results.map((character, index) => <li key={index}>{character?.name}</li>)}
+      </ul>
     </div>
   );
 };
 
 export default CharactersPage;
-
-export async function getStaticProps() {
-  const { data } = await client.query({
-    query: gql`
-      query Characters {
-        characters(filter: { name: "Mechanical Rick" }) {
-          results {
-            id
-            name
-            status
-          }
-        }
-      }
-    `,
-  });
-
-  return {
-    props: {
-      characters: data.characters.results,
-    },
-  };
-}
