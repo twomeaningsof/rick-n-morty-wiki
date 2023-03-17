188ccd37391e354d932ceff30f08d74bd78c7a35
mecies <maciekhnat@gmail.com>
2023-03-17 10:35:55 +0100

    make life easier

--- src/pages/characters/character/[characterID].tsx
+++ src/pages/characters/character/[characterID].tsx
@@ -29,12 +29,20 @@ const GET_CHARACTER_QUERY = graphql(/* GraphQL */ `
   }
 `);
 
-const CharacterPage = ({ characterID }: { characterID: string }) => {
+const useCharacter = ({ characterID }: { characterID: string }) => {
   const { data, loading, error } = useQuery(GET_CHARACTER_QUERY, {
     variables: {
       id: characterID,
     },
   });
+
+  return { character: data?.character, episodesTheyPlayIn: data?.character?.episode || [], loading, error };
+};
+
+const CharacterPage = ({ characterID }: { characterID: string }) => {
+  const { character, episodesTheyPlayIn, loading, error } = useCharacter({ characterID });
+
+  if (!character) return null;
 
   return (
     <div className='w-full min-h-full flex flex-col bg-black/[.85] font-mali text-white [background-image:url("../../public/endless-constellation.svg")]'>
@@ -62,29 +70,27 @@ const CharacterPage = ({ characterID }: { characterID: string }) => {
           <div className='mt-10 flex flex-col sm:ml-10 sm:flex-row sm:items-center'>
             <div className='max-w-[350px] mb-6 flex flex-col items-center self-center justify-center'>
               <img
-                src={data?.character?.image || undefined}
+                src={character.image || undefined}
                 alt={"character img"}
                 className={
                   "w-[200px] h-[200px] sm:w-auto sm:h-auto rounded-full border-[1px] border-[#bfd84d] drop-shadow-[0px_0px_20px_#12b0c9]"
                 }
               />
               <div className='mt-6 text-4xl tracking-wide text-center text-[#bfd84d] drop-shadow-[0px_0px_2px_#12b0c9]'>
-                {data?.character?.name}
+                {character.name}
               </div>
             </div>
             <div className='mb-3 sm:mb-20 mx-11 flex-start'>
               <CharacterDetailParagraph label='ID' value={characterID} />
-              <CharacterDetailParagraph label='Status' value={data?.character?.status} />
-              <CharacterDetailParagraph label='Gender' value={data?.character?.gender} />
-              <CharacterDetailParagraph label='Last known location' value={data?.character?.location?.name} />
+              <CharacterDetailParagraph label='Status' value={character.status} />
+              <CharacterDetailParagraph label='Gender' value={character.gender} />
+              <CharacterDetailParagraph label='Last known location' value={character.location?.name} />
             </div>
           </div>
           <div className='mt-2 mx-10'>
             <span className='text-lg text-[#12b0c9] drop-shadow-[0px_0px_1px_#bfd84d] font-medium'>EPISODES</span>
             <ul className='mt-2 flex flex-wrap justify-start items-start'>
-              {data?.character?.episode.map((episode, id) =>
-                episode?.name ? <EpisodeTag name={episode.name} key={id} /> : null
-              )}
+              {episodesTheyPlayIn.map((episode, id) => episode?.name && <EpisodeTag name={episode.name} key={id} />)}
             </ul>
           </div>
         </>
